22e3344b83fdad75a76c67f806ef72a1ca3ffbf4
stampede247 <robbitay@gmail.com>
2014-09-19 14:31:30 -0700

    Calc Additions

--- Calc.cs
+++ Calc.cs
@@ -54,7 +54,75 @@ namespace OpenTKTest
         {
             return (float)(Math.Pow(p2.X - p1.X, 2) + Math.Pow(p2.Y - p1.Y, 2));
         }
+        /// <summary>
+        /// Returns a point on the circle in the direction 'rotation' with radius
+        /// </summary>
+        /// <param name="rotation">In radians</param>
+        /// <param name="radius">radius of the circle</param>
+        /// <returns></returns>
+        public static Vector2 CirclePoint(float rotation, float radius)
+        {
+            return new Vector2(radius * (float)Math.Cos(rotation), radius * (float)Math.Sin(rotation));
+        }
+        /// <summary>
+        /// Finds the rotational gap between two angles
+        /// Solves the 0 = 360 degree problem
+        /// e.g. 1 degree and 359 degrees will return 2 degrees
+        /// </summary>
+        /// <param name="angle1"></param>
+        /// <param name="angle2"></param>
+        /// <param name="inRadians">Whether or not to work in radians</param>
+        /// <returns>Difference from angle1 to angle2 (Not absolute value)</returns>
+        public static float AngleDifference(float angle1, float angle2, bool inRadians = true)
+        {
+            float full = (inRadians ? MathHelper.Pi * 2 : 360);
+            float a1 = angle1;
+            float a2 = angle2;
+            if (a1 > full)
+                a1 -= full;
+            if (a1 < 0)
+                a1 += full;
+            if (a2 > full)
+                a2 -= full;
+            if (a2 < 0)
+                a2 += full;
 
+            if (Math.Abs(a1 - a2) > full / 2f)
+            {
+                a2 -= full;
+            }
+
+            return a2 - a1;
+        }
+        /// <summary>
+        /// Simple helper to return whether value falls inside bounds
+        /// </summary>
+        public static bool WithinBounds(float p1, float p2, float value)
+        {
+            return (Math.Min(p1, p2) < value && value < Math.Max(p1, p2));
+        }
+        /// <summary>
+        /// Helper function to determine if two "bounded regions" on an axis intersect each other
+        /// </summary>
+        /// <param name="inclusive">Whether or not same ending points inflict and intersection</param>
+        public static bool BoundsIntersect(float p1, float p2, float p3, float p4, bool inclusive = true)
+        {
+            float min1 = Math.Min(p1, p2);
+            float max1 = Math.Max(p1, p2);
+            float min2 = Math.Min(p3, p4);
+            float max2 = Math.Max(p3, p4);
+
+            if (inclusive)
+            {
+                return (min1 <= max2 && max1 >= min2);
+            }
+            else
+            {
+                return (min1 < max2 && max1 > min2);
+            }
+        }
+
+        #region Longer Functions
         /// <summary>
         /// Does a continuous collision check between two Rectangles (one of them moving, other static)
         /// </summary>
@@ -251,5 +319,171 @@ namespace OpenTKTest
             solution = new RectangleF(solEndPosition.X, solEndPosition.Y, startRec.Width, startRec.Height);
             return true;
         }
+
+        /// <summary>
+        /// Tests for intersection between two lines
+        /// </summary>
+        /// <param name="l1P1">First point of line 1</param>
+        /// <param name="l1P2">Second point of line 1</param>
+        /// <param name="l2P1">First point of line 2</param>
+        /// <param name="l2P2">Second point of line 2</param>
+        /// <param name="intersection">Point where the two lines intersect. Vector2.Zero if no intersection</param>
+        /// <returns>wether it found an intersection</returns>
+        public static bool LineVLine(Vector2 l1P1, Vector2 l1P2, Vector2 l2P1, Vector2 l2P2, out Vector2 intersection)
+        {
+            if (l1P1.X != l1P2.X)
+            {
+                //This is the formula this is based off of
+                //E = B-A = ( Bx-Ax, By-Ay )
+                //F = D-C = ( Dx-Cx, Dy-Cy ) 
+                //P = ( -Ey, Ex )
+                //h = ( (A-C) * P ) / ( F * P )
+
+                Vector2 A = l1P1;
+                Vector2 B = l1P2;
+                Vector2 C = l2P1;
+                Vector2 D = l2P2;
+                Vector2 E = B - A;
+                Vector2 F = D - C;
+                Vector2 P = new Vector2(-E.Y, E.X);
+                float H = Vector2.Dot((A - C), P) / Vector2.Dot(F, P);
+                if (0 <= H && H <= 1)
+                {
+                    Vector2 ret = (l2P1 + (l2P2 - l2P1) * H);
+                    float H2 = (ret.X - l1P1.X) / (l1P2.X - l1P1.X);
+                    if (0 <= H2 && H2 <= 1)
+                    {
+                        intersection = ret;
+                        return true;
+                    }
+                }
+            }
+            else
+            {
+                //This is a handler for when the slope of l1 is undefined
+                //I can't remember what this if was for but removing it fixed some problems..
+                if (l2P1.X == l2P2.X)
+                {
+                    //Parrallell lines 
+                    if (l2P1.X == l1P1.X && BoundsIntersect(l2P1.Y, l2P2.Y, l1P1.Y, l1P2.Y, true))
+                    {
+                        if (WithinBounds(l1P1.Y, l1P2.Y, l2P1.Y))
+                        {
+                            intersection = l2P1;
+                        }
+                        else if (WithinBounds(l1P1.Y, l1P2.Y, l2P2.Y))
+                        {
+                            intersection = l2P2;
+                        }
+                        else
+                        {
+                            intersection = (l1P1 + l1P2) / 2f;
+                        }
+                        return true;
+                    }
+                }
+                if (WithinBounds(l2P1.X, l2P2.X, l1P1.X))
+                {
+                    float height = ((float)(l2P2.Y - l2P1.Y) / (float)(l2P2.X - l2P1.X)) * (l1P1.X - l2P1.X);
+                    height += l2P1.Y;
+                    if (l1P1.Y <= height && l1P2.Y >= height)
+                    {
+                        intersection = new Vector2(l1P1.X, height);
+                        return true;
+                    }
+                    else if (l1P1.Y >= height && l1P2.Y <= height)
+                    {
+                        intersection = new Vector2(l1P1.X, height);
+                        return true;
+                    }
+                }
+            }
+            intersection = Vector2.Zero;
+            return false;
+        }
+
+        /// <summary>
+        /// Tests for intersection between a line and a rectangle
+        /// </summary>
+        /// <param name="rec">First point of line 1</param>
+        /// <param name="l2P1">First point of line 2</param>
+        /// <param name="l2P2">Second point of line 2</param>
+        /// <param name="intersection">Point where the two intersect. Vector2.Zero if no intersection</param>
+        /// <returns>wether it found an intersection</returns>
+        public static bool LineVRectangle(RectangleF rec, Vector2 l1P1, Vector2 l1P2, out Vector2 intersection1, out Vector2? intersection2)
+        {
+            Vector2 rP1 = new Vector2(rec.X, rec.Y);
+            Vector2 rP2 = new Vector2(rec.Right, rec.Y);
+            Vector2 rP3 = new Vector2(rec.Right, rec.Bottom);
+            Vector2 rP4 = new Vector2(rec.X, rec.Bottom);
+            Vector2 inter = Vector2.Zero;
+            intersection1 = Vector2.Zero;
+            intersection2 = null;
+            bool found1 = false;
+            if (LineVLine(rP1, rP2, l1P1, l1P2, out inter))
+            {
+                if (!found1)
+                {
+                    intersection1 = inter;
+                    found1 = true;
+                }
+                else
+                {
+                    intersection2 = inter;
+                }
+            }
+            if (LineVLine(rP2, rP3, l1P1, l1P2, out inter))
+            {
+                if (!found1)
+                {
+                    intersection1 = inter;
+                    found1 = true;
+                }
+                else
+                {
+                    intersection2 = inter;
+                }
+            }
+            if (LineVLine(rP3, rP4, l1P1, l1P2, out inter))
+            {
+                if (!found1)
+                {
+                    intersection1 = inter;
+                    found1 = true;
+                }
+                else
+                {
+                    intersection2 = inter;
+                }
+            }
+            if (LineVLine(rP4, rP1, l1P1, l1P2, out inter))
+            {
+                if (!found1)
+                {
+                    intersection1 = inter;
+                    found1 = true;
+                }
+                else
+                {
+                    intersection2 = inter;
+                }
+            }
+
+            if (found1 && intersection2.HasValue)
+            {
+                if (Distance(intersection1, l1P1) > Distance(intersection2.Value, l1P1))
+                {
+                    Vector2 o = intersection1;
+                    intersection1 = intersection2.Value;
+                    intersection2 = o;
+                }
+            }
+
+            if (found1)
+                return true;
+            else
+                return false;
+        }
+        #endregion
     }
 }
